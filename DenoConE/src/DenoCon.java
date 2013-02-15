83c4b5b56dbe8cbedede2317f7b6252112967426
BRunner077 <nasmazani@gmail.com>
2013-02-15 22:28:50 +0100

    Update DenoConE/src/DenoCon.java
    Odstraněny zbytečné importy.

--- DenoConE/src/DenoCon.java
+++ DenoConE/src/DenoCon.java
@@ -1,8 +1,5 @@
-
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.ScrolledComposite;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
 import org.eclipse.swt.layout.GridData;
